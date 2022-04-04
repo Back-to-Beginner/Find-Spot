dcab899fbefef7b9a9b48051fbc6e17c6e7f7921
0BVer <dr0joon@gmail.com>
2022-04-04 17:02:28 +0900

    feat : add S3 uploads
    need test and debug

---
+++ backend/src/main/java/com/backend/controller/S3Controller.java
@@ -0,0 +1,23 @@
+package com.backend.controller;
+
+import com.backend.component.S3Uploader;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@RestController
+public class S3Controller {
+
+    private final S3Uploader s3Uploader;
+
+    @PostMapping("/images")
+    public String upload(@RequestParam("images") MultipartFile multipartFile) throws IOException {
+        s3Uploader.upload(multipartFile, "static");
+        return "test";
+    }
+}
