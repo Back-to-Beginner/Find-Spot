dd75ec186025bc889dacd820346a34702e45f18e
0BVer <dr0joon@gmail.com>
2022-04-11 16:47:01 +0900

    feat : create JpaImageRepository to save Image path by Jpa

---
+++ backend/src/main/java/repository/JpaImageRepository.java
@@ -0,0 +1,41 @@
+package repository;
+
+import com.backend.entity.Image;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+public class JpaImageRepository implements ImageRepository {
+
+    private final EntityManager em;
+
+    public JpaImageRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public Image save(Image image) {
+        em.persist(image);
+        return image;
+    }
+
+    @Override
+    public Optional<Image> findById(Long id) {
+        Image image = em.find(Image.class, id);
+        return Optional.ofNullable(image);
+    }
+
+    @Override
+    public List<Image> findByLocationId(Long location_id) {
+        return em.createQuery("select m from Image m where m.location = :location_id", Image.class)
+                .setParameter("location_id", location_id)
+                .getResultList();
+    }
+
+    @Override
+    public List<Image> findAll() {
+        return em.createQuery("select m from Image m", Image.class)
+                .getResultList();
+    }
+}
