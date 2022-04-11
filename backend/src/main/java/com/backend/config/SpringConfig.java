d60736d801a5681da64e016f812edac584134c4f
0BVer <dr0joon@gmail.com>
2022-04-11 16:38:40 +0900

    chore : edit format

--- backend/src/main/java/com/backend/config/SpringConfig.java
+++ backend/src/main/java/com/backend/config/SpringConfig.java
@@ -1,31 +1,33 @@
 package com.backend.config;
 
 import com.backend.service.UserService;
-import com.backend.user.JpaUserRepository;
-import com.backend.user.UserRepository;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
+import repository.JpaUserRepository;
+import repository.UserRepository;
+
 @Configuration
 public class SpringConfig {
-  private final DataSource dataSource;
-  private final EntityManager em;
+	private final DataSource dataSource;
+	private final EntityManager em;
 
-  public SpringConfig(DataSource dataSource, EntityManager em) {
-    this.dataSource = dataSource;
-    this.em = em;
-  }
+	public SpringConfig(DataSource dataSource, EntityManager em) {
+		this.dataSource = dataSource;
+		this.em = em;
+	}
 
-  @Bean
-  public UserService userService() {
-    return new UserService((JpaUserRepository) userRepository());
-  }
+	@Bean
+	public UserService userService() {
+		return new UserService((JpaUserRepository)userRepository());
+	}
 
-  @Bean
-  public UserRepository userRepository() {
-    return new JpaUserRepository(em);
-  }
+	@Bean
+	public UserRepository userRepository() {
+		return new JpaUserRepository(em);
+	}
 }
