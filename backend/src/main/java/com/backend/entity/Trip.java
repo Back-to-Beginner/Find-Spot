82ec72d175b9d72053c8b86ebedeafbbb2f52e9e
0BVer <dr0joon@gmail.com>
2022-04-14 10:52:06 +0900

    feat : 트립 관련 서비스, 컨트롤러, 리포지토리 생성

--- backend/src/main/java/com/backend/entity/Trip.java
+++ backend/src/main/java/com/backend/entity/Trip.java
@@ -4,8 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -31,4 +29,14 @@ public class Trip extends BaseTimeEntity {
 	private int fullCost;
 
 
+	@Override
+	public String toString() {
+		return "Trip{" +
+				"id=" + id +
+				", title='" + title + '\'' +
+				", text='" + text + '\'' +
+				", date=" + date +
+				", fullCost=" + fullCost +
+				'}';
+	}
 }
