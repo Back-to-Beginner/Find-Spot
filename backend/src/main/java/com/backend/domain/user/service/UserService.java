060220c124eb00bd2ee875af9052df3ddd66f2e3
0BVer <dr0joon@gmail.com>
2022-04-28 16:23:42 +0900

    refactor : 유저 관련 패키지 리팩토링

--- backend/src/main/java/com/backend/domain/user/service/UserService.java
+++ backend/src/main/java/com/backend/domain/user/service/UserService.java
@@ -1,46 +1,4 @@
 package com.backend.domain.user.service;
 
-import com.backend.domain.user.domain.entity.User;
-
-import javax.transaction.Transactional;
-
-import java.util.List;
-import java.util.Optional;
-
-@Transactional
 public class UserService {
-    private final UserRepository userRepository;
-
-    public UserService(JpaUserRepository jpaUserRepository) {
-        this.userRepository = jpaUserRepository;
-    }
-
-    //로그인
-    public boolean login() {
-
-        return false;
-    }
-
-    // 회원 가입
-    public Long join(User user) {
-        validateDuplicateMember(user);
-        userRepository.save(user);
-        return user.getId();
-    }
-
-    private void validateDuplicateMember(User user) {
-        userRepository.findByName(user.getName())
-                .ifPresent(m -> {
-                    throw new IllegalStateException("이미 존재하는 회원입니다.");
-                });
-    }
-
-    // 전체 회원 조회
-    public List<User> findMembers() {
-        return userRepository.findAll();
-    }
-
-    public Optional<User> findOne(Long memberId) {
-        return userRepository.findById(memberId);
-    }
 }
