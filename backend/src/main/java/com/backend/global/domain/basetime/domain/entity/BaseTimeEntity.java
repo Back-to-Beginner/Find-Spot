d292d1130ab815497a7ac38c69b2c52ba3ea1dd9
0BVer <dr0joon@gmail.com>
2022-05-16 04:17:31 +0900

    debug : createdAt, updatedAt 기능 버그 해결

--- backend/src/main/java/com/backend/global/domain/basetime/domain/entity/BaseTimeEntity.java
+++ backend/src/main/java/com/backend/global/domain/basetime/domain/entity/BaseTimeEntity.java
@@ -2,28 +2,24 @@ package com.backend.global.domain.basetime.domain.entity;
 
 import java.time.LocalDateTime;
 
-import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import lombok.Getter;
 
-//TODO 작동을 하지 않음
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseTimeEntity {
 
-    @CreatedDate
-    @Column(name = "crated_date")
-    private LocalDateTime createdDate;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
-    @LastModifiedDate
-    @Column(name = "modified_date")
-    private LocalDateTime modifiedDate;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 
 }
