bd3940b20be2d0d696b8c2e02cceb25ef4bd8f6d
0BVer <dr0joon@gmail.com>
2022-05-22 13:29:25 +0900

    debug : fix syntax

--- backend/scripts/health.sh
+++ backend/scripts/health.sh
@@ -12,7 +12,7 @@ IDLE_PORT=$(find_idle_port)
 
 echo "> Health Check Start!"
 echo "> IDLE_PORT: $IDLE_PORT"
-echo "> curl -s http://localhost:$IDLE_PORT/profile "
+echo "> curl -s http://localhost:$IDLE_PORT/profile"
 sleep 10
 
 for RETRY_COUNT in {1..10}
--- backend/scripts/start.sh
+++ backend/scripts/start.sh
@@ -13,7 +13,7 @@ PROJECT_NAME=backend
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/deploy/*.jar $REPOSITORY/"
 
-cp $REPOSITORY/deloy/*.jar $REPOSITORY/
+cp $REPOSITORY/deploy/*.jar $REPOSITORY/
 
 echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
