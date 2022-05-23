c0d042ed34b66974d56e1b1e2a2ef39d6a73688f
0BVer <dr0joon@gmail.com>
2022-05-23 14:07:48 +0900

    feat : fix code deploy stop springboot

--- backend/scripts/stop.sh
+++ backend/scripts/stop.sh
@@ -3,13 +3,14 @@
 # stop.sh
 # 서버 중단을 위한 스크립트
 
-ABSPATH=$(readlink -f $0)
+#ABSPATH=$(readlink -f $0)
 # ABSDIR : 현재 stop.sh 파일 위치의 경로
-ABSDIR=$(dirname $ABSPATH)
+#ABSDIR=$(dirname $ABSPATH)
 # import profile.sh
-source ${ABSDIR}/profile.sh
+#source ${ABSDIR}/profile.sh
 
-IDLE_PORT=$(find_idle_port)
+#IDLE_PORT=$(find_idle_port)
+IDLE_PORT=8080
 
 echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
 IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
