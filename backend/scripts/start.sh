880c65d79a4de675048f33a57185640ddfdc08ab
0BVer <dr0joon@gmail.com>
2022-05-23 09:02:41 +0900

    feat : remove blue green deploy

--- backend/scripts/start.sh
+++ backend/scripts/start.sh
@@ -30,10 +30,12 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-IDLE_PROFILE=$(find_idle_profile)
+#IDLE_PROFILE=$(find_idle_profile)
+#
+#echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
+#nohup java -jar \
+#    -Dspring.config.location=classpath:/application.yml,classpath:/application-$IDLE_PROFILE.yml \
+#    -Dspring.profiles.active=$IDLE_PROFILE \
+#    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
-echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
-nohup java -jar \
-    -Dspring.config.location=classpath:/application.yml,classpath:/application-$IDLE_PROFILE.yml \
-    -Dspring.profiles.active=$IDLE_PROFILE \
-    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar backend-0.0.1-SNAPSHOT.jar
