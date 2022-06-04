4cdf3d47baaee69f9e974044cd9c5e222ca0ddb4
0BVer <dr0joon@gmail.com>
2022-06-04 23:50:06 +0900

    hotfix : 리퀘스트 바디 어노테이션이 인식에 문제가 있어서 일부 코드에서 삭제

--- backend/src/main/java/com/backend/domain/user/controller/UserController.java
+++ backend/src/main/java/com/backend/domain/user/controller/UserController.java
@@ -36,7 +36,7 @@ public class UserController {
 
     @PostMapping
     @ResponseStatus(CREATED)
-    public ApiResponse createUser(@Validated @RequestBody UserRequest userRequest) {
+    public ApiResponse createUser(@Validated UserRequest userRequest) {
         return created(
                 UserResponse.of(
                         userService.createUser(
@@ -58,8 +58,7 @@ public class UserController {
 
     @GetMapping("/login")
     @ResponseStatus(OK)
-    //TODO RequestBody 를 스프링웹 기본 어노테이션으로 처리하였을 때 스웨거에서 리퀘스트 파라미터로 처리하는 버그
-    public ApiResponse loginUser(@Validated @io.swagger.v3.oas.annotations.parameters.RequestBody LoginRequest request) {
+    public ApiResponse loginUser(@Validated LoginRequest request) {
         return Stream.of(request)
                 .map(userService::login)
                 .map(UserResponse::of)
