d8c2af626d48087e88a34216fdb7d8f53e83cf88
daehyeon <eogus0287@naver.com>
2022-09-26 19:26:15 +0900

    chore : 코드 정렬

--- frontend/src/components/box/RegisterBox.js
+++ frontend/src/components/box/RegisterBox.js
@@ -7,28 +7,28 @@ import PasswordConfirmInput from '../input/PasswordConfirmInput';
 const RegisterBox = () => {
 
     return <>
-    <div className="registerformBox">
-        <div className='registerBoxTitle'>
-            Register
+        <div className="registerformBox">
+            <div className='registerBoxTitle'>
+                Register
+            </div>
+            <form>
+                <div style={{ marginTop: '67px', marginLeft: '50px', marginRight: '50px' }}>
+                    <EmailInput />
+                </div>
+                <div style={{ marginTop: '20px', marginLeft: '50px', marginRight: '50px' }}>
+                    <PasswordInput />
+                </div>
+                <div style={{ marginTop: '20px', marginLeft: '50px', marginRight: '50px' }}>
+                    <PasswordConfirmInput />
+                </div>
+                <button style={{ marginTop: '105px', marginLeft: '50px', marginRight: '50px' }}>
+                    <YellowButton buttonName={'Confirm'} />
+                </button>
+            </form>
+            <div className='login'>
+                Login
+            </div>
         </div>
-        <form>
-        <div style={{marginTop: '67px', marginLeft: '50px', marginRight: '50px'}}>
-        <EmailInput/>
-        </div>
-        <div style={{marginTop: '20px', marginLeft: '50px', marginRight: '50px'}}>
-        <PasswordInput/>
-        </div>
-        <div style={{marginTop: '20px', marginLeft: '50px', marginRight: '50px'}}>
-            <PasswordConfirmInput/>
-        </div>
-    <button style={{marginTop: '105px', marginLeft: '50px', marginRight: '50px'}}>
-        <YellowButton buttonName={'Confirm'}/>
-    </button>
-    </form>
-    <div className='login'>
-        Login
-    </div>
-    </div>
     </>
 }
 
