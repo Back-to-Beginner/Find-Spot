723b89c26624503bbacb4f80dfe715edf429ce1a
daehyeon <eogus0287@naver.com>
2022-10-01 15:45:55 +0900

    feat: 디테일 뷰 화면 수정

--- frontend/src/pages/detailView/DetailVieiw.js
+++ frontend/src/pages/detailView/DetailVieiw.js
@@ -1,24 +1,33 @@
 import React from "react";
-import MissionCard from "../../components/cards/missionCard/MissionCard";
+import SmallMissionCard from "../../components/cards/missionCard/SmallMissionCard";
+import SmallSuccessCard from "../../components/cards/successCard/SmallSuccessCard";
 import CommentCard from "../../components/cards/commentCard/CommentCard";
 import YellowButton from "../../components/button/YelloButton";
 
 const DetailView = () => {
 
     return <>
-        <div className='background'>
-            <div className='missionDetailViewEdge'>
-                <MissionCard />
+        <div className='background' >
+            <div className='detailViewLocation'>
+                <div style={{ padding: '10px' }}>
+                    <SmallMissionCard /> {/*SmallMissionCard */}
+                </div>
+                <div style={{ padding: '10px' }}>
+                    <SmallSuccessCard />  {/*SmallCommentCard */}
+                </div>
             </div>
-            <div className='CommentViewEdge'>
-                <CommentCard />
+            <div className="mainBoxPosition">
+                <div style={{ paddingTop: '50px' }}>
+                    <YellowButton buttonName={'Challenge!'} />
+                </div>
+                <div style={{ padding: '10px' }}>
+                    <div className='findSuccess'>
+                        Find Success
+                    </div>
+                </div>
             </div>
-            <div className="detailBoxPosition">
-                <YellowButton buttonName={'Challenge!'} />
-            </div>
-            <div className='findSuccess'>
-                Find Success
-            </div>
+
+
         </div>
     </>
 }
