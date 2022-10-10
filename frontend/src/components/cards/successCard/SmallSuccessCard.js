80b6cddc6cdb0794976727870e7f8ff19cf247df
daehyeon <eogus0287@naver.com>
2022-10-10 15:56:46 +0900

    feat : 호버 효과 적용(마우스 올릴 시 "자세히 보기" 칸 출력)

--- frontend/src/components/cards/successCard/SmallSuccessCard.js
+++ frontend/src/components/cards/successCard/SmallSuccessCard.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import '../../../css/common.css'
 
 const SmallSuccessCard = (props) => {
@@ -6,17 +6,25 @@ const SmallSuccessCard = (props) => {
     const [imageSrc, setImageSrc] = useState(props.image);
 
     const getImage = () => {
-      return imageSrc ? imageSrc : null;
+        return imageSrc ? imageSrc : null;
     }
 
     return <>
         <div className={'smallSuccessView'}>
-            <div className={'smallSuccessImageMask'}>
-                <img className={'smallSuccessImage'}
-                     src={getImage()}
-                     alt={null}/>
-            </div>
-
+            <ul>
+                <li>
+                    <a href="#">
+                        <figure>
+                            <div className={'smallSuccessImageMask'}>
+                                <img className={'smallSuccessImage'}
+                                    src={getImage()}
+                                    alt={null} />
+                            </div>
+                            <figcaption>자세히보기</figcaption>
+                        </figure>
+                    </a>
+                </li>
+            </ul>
             <div className={'smallSuccessCardContent'}>
                 {successContent}
             </div>
